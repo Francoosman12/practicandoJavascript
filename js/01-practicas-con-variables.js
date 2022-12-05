d44df10ad65d5730ede809c3c547b97a392df177
francoosman12 <francoosman12@gmail.com>
2022-12-05 16:39:21 -0300

    Avanzamos a Bucles

--- js/01-practicas-con-variables.js
+++ js/01-practicas-con-variables.js
@@ -21,17 +21,74 @@
 //Nota: Tener en cuenta la siguiente función: parseInt
 //https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/parseInt
 
-let num1 = parseInt(prompt("Ingrese el primer numero:"));
+/*let num1 = parseInt(prompt("Ingrese el primer numero:"));
 let num2 = parseInt(prompt("Ingrese el segundo numero:"));
 document.write(
   `El resultado de la suma de los numeros ingresado es ${num1 + num2}`
-);
+);*/
 
 //6.- Escribe un programa que pida dos números y escriba en la pantalla cual es el mayor.
+
+/*let num1 = parseInt(prompt("Ingrese el primer número: "));
+let num2 = parseInt(prompt("Ingrese el segundo número: "));
+if (num1 > num2) {
+  document.write(`El número ${num1} es mayor que ${num2}`);
+} else document.write(`El número ${num2} es mayor que ${num1}`);*/
+
 //7.- Escribe un programa que pida 3 números y escriba en la pantalla el mayor de los tres.
+
+/*let num1 = parseInt(prompt("Ingrese el primer número: "));
+let num2 = parseInt(prompt("Ingrese el segundo número: "));
+let num3 = parseInt(prompt("Ingrese el tercer número: "));
+
+let mayor = Math.max(num1, num2, num3);
+
+document.write(`El mayor de los números ingresados es ${mayor}`);*/
+
 //8.- Escribe un programa que pida un número y diga si es divisible por 2.
+
+/*let num = parseInt(prompt("Ingrese un número para ver si es divisible por 2:"));
+
+if (num % 2 === 0) {
+  alert(`El número ${num} es divisible por 2.`);
+} else {
+  alert(`El número ${num} no es divisible por dos`);
+}*/
+
 //9.- Escribe un programa que pida una frase y escriba las vocales que aparecen
 //Nota: Tener en cuenta la función length y substring o charAt (developer mozilla)
 
-//10.- Escribe un programa que pida un número y nos diga si es divisible por 2, 3, 5 o 7 (sólo hay que comprobar si lo es por uno de los cuatro.
+/*let frase = prompt("Escribe una frase:");
+
+document.write(
+  `La frase ${frase} tiene las siguientes vocales: ${frase.charAt(
+    a,
+    e,
+    i,
+    o,
+    u
+  )} `
+);*/
+
+//10.- Escribe un programa que pida un número y nos diga si es divisible por 2, 3, 5 o 7 (sólo hay que comprobar si lo es por uno de los cuatro).
+
+//let num = prompt("Ingrese un numero:");
+
 //11.- Añadir al ejercicio anterior que nos diga por cuál de los cuatro es divisible (hay que decir todos por los que es divisible).
+
+/*let num = prompt("Ingrese un numero:");
+
+if (num % 2 === 0) {
+  console.log("El numero es divisible por 2");
+}
+if (num % 3 === 0) {
+  console.log("El numero es divisible por 3");
+}
+if (num % 5 === 0) {
+  console.log("El numero es divisible por 5");
+}
+if (num % 7 === 0) {
+  console.log("El numero es divisible por 7");
+} else {
+  console.log("El numero no es divisible por ninguno de los divisores!");
+}*/
